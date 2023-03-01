61cc3dde3547cf1a61a49c981e6b3e91c773e7c8
malyszaryczlowiek <45798727+malyszaryczlowiek@users.noreply.github.com>
2023-03-01 21:20:02 +0100

    SessionInfo encoder and decoder added.

--- scala-2.12/src/main/scala/kessengerlibrary/model/SessionInfo.scala
+++ scala-2.12/src/main/scala/kessengerlibrary/model/SessionInfo.scala
@@ -1,6 +1,40 @@
 package io.github.malyszaryczlowiek
 package kessengerlibrary.model
 
+import io.circe.Decoder.Result
+import io.circe.parser.decode
+import io.circe.syntax.EncoderOps
+import io.circe.{Decoder, Encoder, Error, HCursor, Json}
+
 import java.util.UUID
 
 case class SessionInfo(sessionId: UUID, userId: UUID, validityTime: Long)
+
+object SessionInfo {
+
+  implicit object encoder extends Encoder[SessionInfo] {
+    override def apply(a: SessionInfo): Json = Json.obj(
+      ("sessionId",    Json.fromString( a.sessionId.toString )),
+      ("userId",       Json.fromString( a.userId.toString    )),
+      ("validityTime", Json.fromLong(   a.validityTime       ))
+    )
+  }
+
+
+  implicit object decoder extends Decoder[SessionInfo] {
+    override def apply(c: HCursor): Result[SessionInfo] = {
+      for {
+        sessionId    <- c.downField("sessionId").as[String]
+        userId       <- c.downField("userId").as[String]
+        validityTime <- c.downField("validityTime").as[Long]
+      } yield {
+        SessionInfo(UUID.fromString(sessionId), UUID.fromString(userId), validityTime)
+      }
+    }
+  }
+
+  def toJSON(m: SessionInfo): String = m.asJson.noSpaces
+
+  def parseSessionInfo(json: String): Either[Error, SessionInfo] = decode[SessionInfo](json)
+
+}
--- scala-3/src/main/scala/kessengerlibrary/model/SessionInfo.scala
+++ scala-3/src/main/scala/kessengerlibrary/model/SessionInfo.scala
@@ -1,6 +1,41 @@
 package io.github.malyszaryczlowiek
 package kessengerlibrary.model
 
+
+import io.circe.Decoder.Result
+import io.circe.parser.decode
+import io.circe.syntax.EncoderOps
+import io.circe.{Decoder, Encoder, Error, HCursor, Json}
+
 import java.util.UUID
 
 case class SessionInfo(sessionId: UUID, userId: UUID, validityTime: Long)
+
+
+
+object SessionInfo {
+
+  given encoder: Encoder[SessionInfo] = (a: SessionInfo) => Json.obj(
+    ("sessionId",    Json.fromString( a.sessionId.toString )),
+    ("userId",       Json.fromString( a.userId.toString    )),
+    ("validityTime", Json.fromLong(   a.validityTime       ))
+  )
+
+
+
+  given decoder: Decoder[SessionInfo] = (c: HCursor) =>
+    for {
+      sessionId    <- c.downField("sessionId").as[String]
+      userId       <- c.downField("userId").as[String]
+      validityTime <- c.downField("validityTime").as[Long]
+    } yield {
+      SessionInfo(UUID.fromString(sessionId), UUID.fromString(userId), validityTime)
+    }
+
+
+
+  def toJSON(m: SessionInfo): String = m.asJson.noSpaces
+
+  def parseSessionInfo(json: String): Either[Error, SessionInfo] = decode[SessionInfo](json)
+
+}
